ea7cd0c4e23cc3c1e18270000271d78b0661c74c
yumlii33 <50021781+yumlii33@users.noreply.github.com>
2019-05-11 12:35:23 +0800

    Add files via upload

---
+++ hw-yuy-20190510.py
@@ -0,0 +1,9 @@
+num_list = list()
+number = 0
+with open( r'log_files\201811123011.log', 'r', encoding='utf8') as f:
+    for line in f:
+        num = line.split(',')[1]
+        num = num[4:]
+        if num == '201811123011':
+            number = number + 1
+print("于梦丽的打卡次数为：{}".format(number))
